3bafc3c399db09d30f6e23dace312d92438fd76b
chengchenrui <380101538@qq.com>
2017-12-12 19:04:08 +0800

    初始

---
+++ src/main/webapp/resource/js/modules/list.js
@@ -0,0 +1,9 @@
+/**
+ * Created by cheng on 2017.12.12.
+ */
+layui.use(['layer', 'form'], function(){
+    var layer = layui.layer
+        ,form = layui.form;
+
+    layer.msg('Hello World');
+});
